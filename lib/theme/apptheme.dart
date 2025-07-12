ce1767254cc7b9d37cdb6231434f60facd91cf46
Enesh <aaa@192.168.1.37>
2025-07-13 01:18:54 +0545

    added the app theme data for app bar and text

---
+++ lib/theme/apptheme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Apptheme {
+  static const String fontFamily = "BubblegumSans";
+  static final ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+
+    //app bar theme
+    appBarTheme: AppBarTheme(
+      titleTextStyle: TextStyle(
+        fontSize: 52,
+        fontFamily: fontFamily,
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+
+    //text theme
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 42),
+      bodyMedium: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+    ),
+  );
+}
